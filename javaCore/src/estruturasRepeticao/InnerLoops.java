e287b311f15f0bee5be0138abfd1222888e2c22b
claudiobelempb <claudio.c.lima@hotmail.com>
2023-08-24 09:29:46 -0300

    update .gitignore

--- javaCore/src/estruturasRepeticao/InnerLoops.java
+++ javaCore/src/estruturasRepeticao/InnerLoops.java
@@ -6,7 +6,7 @@ public class InnerLoops {
 		
 		outer:
 		for(int i = 1; i <= 10; i++) {
-			inner:
+		
 			for(int j = 1; j <= 10; j++) {
 				if(j == 5) break outer;
 				System.out.println(i + j);
