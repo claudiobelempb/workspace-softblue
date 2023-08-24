f49e869b22ef3781fa8c04150187d545b9f3d12e
claudiobelempb <claudio.c.lima@hotmail.com>
2023-08-24 08:53:33 -0300

    Forçando o teste do loop com o continue

---
+++ javaCore/src/estruturasRepeticao/RepeatContinue.java
@@ -0,0 +1,18 @@
+package estruturasRepeticao;
+
+public class RepeatContinue {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 200; i++) {
+
+			if (i % 2 != 0)
+				continue;
+
+			System.out.println(i);
+		}
+
+		System.out.println("FIM");
+
+	}
+
+}
