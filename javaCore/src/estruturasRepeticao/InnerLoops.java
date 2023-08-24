cc01fdc87a1bb243978351632c0df0629be59ef5
claudiobelempb <claudio.c.lima@hotmail.com>
2023-08-24 09:08:09 -0300

    Loops dentro de loops

---
+++ javaCore/src/estruturasRepeticao/InnerLoops.java
@@ -0,0 +1,19 @@
+package estruturasRepeticao;
+
+public class InnerLoops {
+
+	public static void main(String[] args) {
+		
+		outer:
+		for(int i = 1; i <= 10; i++) {
+			inner:
+			for(int j = 1; j <= 10; j++) {
+				if(j == 5) break outer;
+				System.out.println(i + j);
+			}
+		}
+		System.out.println("FIM");
+	}
+	
+
+}
